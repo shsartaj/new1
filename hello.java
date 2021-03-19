f8707dde7984d77633841a3e7552bb907dd7055f
shsartaj <80391093+shsartaj@users.noreply.github.com>
2021-03-19 15:25:55 +0530

    updating 2nd time

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
     public static void main(String[] args) {
-        for(int i=0;i<=10;i++)
-        System.out.println("Hi Welcome hii"+i);
+        for(int i=0;i<=12;i++)
+        System.out.println("Hi Welcome saru hii"+i);
     }
 
 }
